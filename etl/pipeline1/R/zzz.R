f19925fffbb8fec167a6b1e178e1cae6feba4fad
Ryan Price <ryapric@gmail.com>
2018-10-06 15:33:49 -0500

    Forgot to finish error message.

--- etl/pipeline1/R/zzz.R
+++ etl/pipeline1/R/zzz.R
@@ -1,6 +1,7 @@
 .onLoad <- function(libname, pkgname) {
     # Stop if not run from correct directory
     if (!file.exists("main.R")) {
-        stop("Script entrypoint 'main.R' requires tjat  not found. Please run from top-level package directory that contains 'main.R'")
+        stop(paste0("Script entrypoint 'main.R' requires that your working directory is the R package top-level directory.\n",
+                    "       Please run from top-level package directory that contains 'main.R'"))
     }
 }
